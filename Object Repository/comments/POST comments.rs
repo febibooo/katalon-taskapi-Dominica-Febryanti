19f23d35be05157a25ba909681ce540b086a5e2c
febibooo <febiboss22@gmail.com>
2024-06-13 11:13:54 +0700

    ganti post comment

--- Object Repository/comments/POST comments.rs
+++ Object Repository/comments/POST comments.rs
@@ -8,25 +8,17 @@
    <smartLocatorEnabled>false</smartLocatorEnabled>
    <useRalativeImagePath>false</useRalativeImagePath>
    <autoUpdateContent>false</autoUpdateContent>
-   <connectionTimeout>-1</connectionTimeout>
+   <connectionTimeout>0</connectionTimeout>
    <followRedirects>false</followRedirects>
    <httpBody></httpBody>
    <httpBodyContent>{
-  &quot;text&quot;: &quot;  {\n    \&quot;postId\&quot;: 31,\n    \&quot;id\&quot;: 31,\n    \&quot;name\&quot;: \&quot;post comments\&quot;,\n    \&quot;email\&quot;: \&quot;gitcoba@gmail.com\&quot;,\n    \&quot;body\&quot;: \&quot;ini adalah body dari post comments\&quot;\n  }&quot;,
+  &quot;text&quot;: &quot;  {\n    \&quot;postId\&quot;: 31,\n    \&quot;id\&quot;: 14,\n    \&quot;name\&quot;: \&quot;post comments\&quot;,\n    \&quot;email\&quot;: \&quot;gitcoba@gmail.com\&quot;,\n    \&quot;body\&quot;: \&quot;ini adalah body dari post comments\&quot;\n  }&quot;,
   &quot;contentType&quot;: &quot;application/json&quot;,
   &quot;charset&quot;: &quot;UTF-8&quot;
 }</httpBodyContent>
    <httpBodyType>text</httpBodyType>
-   <httpHeaderProperties>
-      <isSelected>true</isSelected>
-      <matchCondition>equals</matchCondition>
-      <name>Content-Type</name>
-      <type>Main</type>
-      <value>application/json</value>
-      <webElementGuid>0f91a929-96ec-4c97-9b1a-c55578bd3ba6</webElementGuid>
-   </httpHeaderProperties>
    <katalonVersion>9.4.0</katalonVersion>
-   <maxResponseSize>-1</maxResponseSize>
+   <maxResponseSize>0</maxResponseSize>
    <migratedVersion>5.4.1</migratedVersion>
    <path></path>
    <restRequestMethod>POST</restRequestMethod>
@@ -37,7 +29,7 @@
    <soapRequestMethod></soapRequestMethod>
    <soapServiceEndpoint></soapServiceEndpoint>
    <soapServiceFunction></soapServiceFunction>
-   <socketTimeout>-1</socketTimeout>
+   <socketTimeout>0</socketTimeout>
    <useServiceInfoFromWsdl>true</useServiceInfoFromWsdl>
    <verificationScript>import static org.assertj.core.api.Assertions.*
 
